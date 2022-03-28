bb7213f05c4d1c3b1e394e407b4ca991950a9ee7
fjl <fangjiale@pudutech.com>
2022-03-28 16:03:20 +0800

    <hotfix>(project): 修改合并分支后 工程无法编译问题
    Signed-off-by: fjl <fangjiale@pudutech.com>

--- Code/platform/function/register_info.h
+++ Code/platform/function/register_info.h
@@ -1,11 +1,20 @@
+/**
+ * @file register_info.h
+ * @author {fangjiale}
+ * @brief
+ * @version 0.1
+ * @date 2022-03-15
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef _REGISTER_INFO_H
 #define _REGISTER_INFO_H
 
 #include "common.h"
-#include "drv_i2c.h"
 #include "drv_pwm.h"
 
-typedef enum ctrl_reg_cmd
+typedef enum
 {
     /**
      * @brief 写灯驱寄存器指令
@@ -56,63 +65,76 @@ typedef enum ctrl_reg_cmd
      * @brief 根据读取寄存器位置获取其后寄存器数量指令
      *
      */
-    POS_GET_REMAIN_REG_NUM,
-    /**
-     * @brief 根据读取寄存器位置获取其后寄存器数量指令
-     *
-     */
     RESET_PARAM_SEG
-}CtrlRegCmdType;
+} ctrl_reg_cmd_type;
 
-#define WS2812_PARA_BASE_ADDR                   (0x13u)
-#define TLC59108_PARA_BASE_ADDR                 (0x03u)
+/**
+ * @brief WS2812/TLC59108寄存器参数区地址偏移
+ *
+ */
+#define WS2812_PARA_BASE_ADDR (0x16u)
+#define TLC59108_PARA_BASE_ADDR (0x02u)
 
-#define CTRL_REG_CMD_CHECK(cmd)                 \
-do {                                            \
-    if (cmd != WR_RGE_INFO &&                   \
-        cmd != RD_REG_INFO &&                   \
-        cmd != RESET_REG_INFO &&                \
-        cmd != GET_TLC59108REG_NUM_INFO &&      \
-        cmd != GET_WS2812REG_NUM_INFO &&        \
-        cmd != GET_REG_NUM_INFO &&              \
-        cmd != GET_REG_DRIVER_TYPE &&           \
-        cmd != GET_TLC59108REG_POS &&           \
-        cmd != GET_WS2812REG_POS &&             \
-        cmd != POS_GET_REMAIN_REG_NUM &&        \
-        cmd != RESET_PARAM_SEG ) {              \
-            goto set_error;                     \
-        }                                       \
-} while(0)
+/**
+ * @brief WS2812/TLC59108灯条工作模式寄存器地址
+ *
+ */
+#define WS2812_WORK_MODE_REG_ADDR (0x13u)
+#define TLC59108_WORK_MODE_REG_ADDR (0x01u)
 
-#define REGADDR_CHECK(cmd, regAddr, regMaxNum)          \
-do {                                                    \
-    if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) &&   \
-        (regAddr == 0 || regAddr > regMaxNum)) {        \
-        goto set_error;                                 \
-    }                                                   \
-} while(0)
+#define CTRL_REG_CMD_CHECK(cmd)                \
+    do                                         \
+    {                                          \
+        if (cmd != WR_RGE_INFO &&              \
+            cmd != RD_REG_INFO &&              \
+            cmd != RESET_REG_INFO &&           \
+            cmd != GET_TLC59108REG_NUM_INFO && \
+            cmd != GET_WS2812REG_NUM_INFO &&   \
+            cmd != GET_REG_NUM_INFO &&         \
+            cmd != GET_REG_DRIVER_TYPE &&      \
+            cmd != GET_TLC59108REG_POS &&      \
+            cmd != GET_WS2812REG_POS &&        \
+            cmd != RESET_PARAM_SEG)            \
+        {                                      \
+            goto set_error;                    \
+        }                                      \
+    } while (0)
 
-#define CTRL_REG_SIZE_CHECK(cmd, size)                  \
-do {                                                    \
-    if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) &&   \
-        size == 0) {                                    \
-            goto set_error;                             \
-        }                                               \
-} while(0)
+#define REGADDR_CHECK(cmd, regAddr, regMaxNum)            \
+    do                                                    \
+    {                                                     \
+        if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) && \
+            (regAddr > regMaxNum - 1))                    \
+        {                                                 \
+            goto set_error;                               \
+        }                                                 \
+    } while (0)
 
-#define CTRL_REG_ARG_CHECK(cmd, arg)                    \
-do {                                                    \
-    if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) &&   \
-        arg == NULL) {                                  \
-            goto set_error;                             \
-    }                                                   \
-} while(0)
+#define CTRL_REG_SIZE_CHECK(cmd, size)                    \
+    do                                                    \
+    {                                                     \
+        if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) && \
+            size == 0)                                    \
+        {                                                 \
+            goto set_error;                               \
+        }                                                 \
+    } while (0)
+
+#define CTRL_REG_ARG_CHECK(cmd, arg)                      \
+    do                                                    \
+    {                                                     \
+        if ((cmd == WR_RGE_INFO || cmd == RD_REG_INFO) && \
+            arg == NULL)                                  \
+        {                                                 \
+            goto set_error;                               \
+        }                                                 \
+    } while (0)
 
 /******************************************************************************/
 /**
  * @brief 初始化灯驱寄存器
  *
- * @return ErrStatus
+ * @return ErrStatus        @SUCCESS:初始化灯驱寄存器成功 @ERROR:初始化灯驱寄存器失败
  */
 ErrStatus init_register(void);
 
@@ -124,8 +146,21 @@ ErrStatus init_register(void);
  * @param arg           指针参数 用于传入读写buff
  * @param size          读写字节数
  * @return ErrStatus
+ *
+ * @note  输入对应操作灯驱寄存器的指令即可操作该寄存器
+ *        控制命令说明:
+ *        @cmd RD_REG_INFO(读寄存器指令) regAddr(寄存器的地址) arg(接收数据地址) size(读取字节数)
+ *             WR_RGE_INFO(写寄存器指令) regAddr(寄存器的地址) arg(写入数据地址) size(写入字节数)
+ *             GET_REG_NUM_INFO(读取灯驱寄存器个数指令) regAddr/size(无效参数) arg(接收数据地址)
+ *             GET_REG_DRIVER_TYPE(获取当前驱动设备类型指令) 同上
+ *             GET_TLC59108REG_NUM_INFO(获取TLC59108灯条寄存器数量指令) 同上
+ *             GET_WS2812REG_NUM_INFO(获取WS2812灯条寄存器数量指令) 同上
+ *             GET_TLC59108REG_POS(获取TLC59108寄存器偏移地址指令) 同上
+ *             GET_WS2812REG_POS(获取WS2812寄存器偏移地址指令) 同上
+ *             RESET_REG_INFO(复位灯驱寄存器所有数据指令) 同上
+ *             RESET_PARAM_SEG(复位不同驱动类型下的灯驱参数指令指令) regAddr(复位寄存器地址) arg/size(无效值)
  */
-ErrStatus control_register(const CtrlRegCmdType cmd,
+ErrStatus control_register(const ctrl_reg_cmd_type cmd,
                            const uint8_t regAddr,
                            void *arg,
                            uint8_t size);
